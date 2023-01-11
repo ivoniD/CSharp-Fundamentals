ad401b88a3d085ea30bd2bf71d6518da7e3721ff
Ivon Dimitrova <117980916+ivoniD@users.noreply.github.com>
2023-01-11 10:40:11 +0200

    Add files via upload

---
+++ Basic Syntax, Conditional Statements and Loops - Lab/02. Passed/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _02._Passed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //•	"Passed!" if the grade is equal or more than 3.00.
+
+            double grade = double.Parse(Console.ReadLine());
+
+            if (grade >= 3)
+            {
+                Console.WriteLine("Passed!");
+            }
+        }
+    }
+}
