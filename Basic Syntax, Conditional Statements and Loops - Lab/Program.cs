3071676c68226c3d50e651860981d63f5fd636c9
Ivon Dimitrova <ivon.p.dimitrova@gmail.com>
2023-01-11 10:35:40 +0200

    Add project files.

---
+++ Basic Syntax, Conditional Statements and Loops - Lab/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Fundamentals_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //•	student name    •	age     •  average grade. 
+            //"Name: {student name}, Age: {student age}, Grade: {student grade}".
+
+            string studentName = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            double averageGrade = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Name: {studentName}, Age: {age}, Grade: {averageGrade:f2}");
+
+        }
+    }
+}
