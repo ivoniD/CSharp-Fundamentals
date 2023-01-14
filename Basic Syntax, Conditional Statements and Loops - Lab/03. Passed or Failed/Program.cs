2d6f23359671e81607ce118085ad7a0e4dd01b03
Ivon Dimitrova <117980916+ivoniD@users.noreply.github.com>
2023-01-14 23:26:53 +0200

    Add files via upload

---
+++ Basic Syntax, Conditional Statements and Loops - Lab/03. Passed or Failed/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _03._Passed_or_Failed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //•	"Passed!" if the grade is equal or more than 3.00. 
+            // or "Failed!" 
+
+            double grade = double.Parse(Console.ReadLine());
+
+            if (grade >= 3)
+            {
+                Console.WriteLine("Passed!");
+            }
+            else
+            {
+                Console.WriteLine("Failed!");
+            }
+        }
+    }
+}
---
+++ Basic Syntax, Conditional Statements and Loops - Lab/03. Vacation/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace _03._Vacation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int numpeople = int.Parse(Console.ReadLine());
+            string type = Console.ReadLine();
+            string day = Console.ReadLine();
+
+            double price = 0;
+            if (type == "Students")
+            //Students   8.45   9.80   10.46
+            //Students >= 30 -> price - 15%
+            {
+                if (day == "Friday")
+                {
+                    price = numpeople * 8.45;
+                }
+                else if (day == "Saturday")
+                {
+                    price = numpeople * 9.80;
+                }
+                else if (day == "Sunday")
+                {
+                    price = numpeople * 10.46;
+                }
+                if (numpeople >= 30)
+                {
+                    price = price - (price * 0.15);
+                }
+            }
+            else if (type == "Business")
+            //Business  10.90   15.60   16
+            //Business >=  100 -> 10 of the people stay for free.
+            {
+                if (day == "Friday")
+                {
+                    price = numpeople * 10.90;
+                    if (numpeople >= 100)
+                    {
+                        price = price - (10 * 10.90);
+                    }
+                }
+                else if (day == "Saturday")
+                {
+                    price = numpeople * 15.60;
+                    if (numpeople >= 100)
+                    {
+                        price = price - (10 * 15.60);
+                    }
+                }
+                else if (day == "Sunday")
+                {
+                    price = numpeople * 16;
+                    if (numpeople >= 100)
+                    {
+                        price = price - (10 * 16);
+                    }
+                }
+
+            }
+            else if (type == "Regular")
+            //Regular   15    20    22.50
+            //Regular >= 10 && <=20 -> price - 5%
+            {
+                if (day == "Friday")
+                {
+                    price = numpeople * 15;
+                }
+                else if (day == "Saturday")
+                {
+                    price = numpeople * 20;
+                }
+                else if (day == "Sunday")
+                {
+                    price = numpeople * 22.50;
+                }
+                if (numpeople >= 10 && numpeople <= 20)
+                {
+                    price = price - (price * 0.05);
+                }
+            }
+            Console.WriteLine($"Total price: {price:f2}");
+
+
+        }
+    }
+}
