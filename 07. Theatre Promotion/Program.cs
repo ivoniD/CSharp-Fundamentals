ed5d9c741c7941859e8f1d8756748b26cf88d87e
Ivon Dimitrova <117980916+ivoniD@users.noreply.github.com>
2023-01-15 22:42:15 +0200

    Add files via upload

---
+++ 07. Theatre Promotion/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace _07._Theatre_Promotion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Day / Age   0 <= age <= 18      18 < age <= 64      64 < age <= 122
+            //Weekday          12$	             18$	             12$
+            //Weekend          15$	             20$	             15$
+            //Holiday           5$	             12$	             10$
+
+            string typeOfDay = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            double price = 0;
+
+            if (age >= 0 && age <= 18)
+            {
+                if (typeOfDay == "Weekday")
+                {
+                    price = 12;
+                }
+                else if (typeOfDay == "Weekend")
+                {
+                    price = 15;
+                }
+                else if (typeOfDay == "Holiday")
+                {
+                    price = 5;
+                }
+            }
+            else if (age > 18 && age <= 64)
+            {
+                if (typeOfDay == "Weekday")
+                {
+                    price = 18;
+                }
+                else if (typeOfDay == "Weekend")
+                {
+                    price = 20;
+                }
+                else if (typeOfDay == "Holiday")
+                {
+                    price = 12;
+                }
+            }
+            else if (age > 64 && age <= 122)
+            {
+                if (typeOfDay == "Weekday")
+                {
+                    price = 12;
+                }
+                else if (typeOfDay == "Weekend")
+                {
+                    price = 15;
+                }
+                else if (typeOfDay == "Holiday")
+                {
+                    price = 10;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Error!");
+                return;
+            }
+
+            Console.WriteLine($"{price}$");
+        }
+    }
+}
