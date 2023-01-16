c4eda9c25710729d72a684b24cdebbf09331a8b2
Ivon Dimitrova <117980916+ivoniD@users.noreply.github.com>
2023-01-16 14:53:27 +0200

    Add files via upload

---
+++ Basic Syntax, Conditional Statements and Loops - Lab/06. Strong number/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace stron_fin
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = Console.ReadLine();
+
+            int factorial = 0;
+
+            for (int i = 0; i <= number.Length - 1; i++)
+            {
+                char currCharacter = number[i];
+                int currDigit = (int)currCharacter - 48;
+
+                int currDigitFactorial = 1;
+                for (int j = currDigit; j >= 1; j--)
+                {
+                    currDigitFactorial = currDigitFactorial * j;
+                }
+                factorial += currDigitFactorial;
+            }
+            if (factorial == int.Parse(number))
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+
+        }
+    }
+}
