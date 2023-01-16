967fc1efe64ae898f6e269cd9b04757ccfaef7e3
Ivon Dimitrova <117980916+ivoniD@users.noreply.github.com>
2023-01-16 14:55:47 +0200

    Add files via upload

---
+++ Basic Syntax, Conditional Statements and Loops - Lab/08. Divisible by 3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _08._Divisible_by_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a program, which prints all the numbers from 1 to 100, that is divisible by 3. You have to use a single for loop. 
+
+            for (int i = 3; i < 100; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+
+            }
+        }
+    }
+}
